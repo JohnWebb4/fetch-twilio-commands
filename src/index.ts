94e524257241f65701e230501a43977d0239c1e1
John Webb <John.Webb94@gmail.com>
2018-11-23 12:19:28 -0700

    Post commands to Google Script URL
    When a command is read it should post to a google
    script url.

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,46 @@
 import dotenv from "dotenv";
+import { UriOptions } from "request";
+import request from "request-promise";
 import twilio from "twilio";
 import { CommandInstance } from "twilio/lib/rest/wireless/v1/command";
+import { logger } from "./util/logger";
 
 dotenv.config();
 
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
+const googleAppKey = process.env.GOOGLE_APP_KEY;
+const googleAppUrl = process.env.GOOGLE_APP_URL;
 
 const client = twilio(accountSid, authToken);
 
 client.wireless.commands.each(undefined, handleCommand);
 
-function handleCommand(command: CommandInstance): void {
+async function handleCommand(command: CommandInstance): Promise<void> {
   if (command.command === "You Pressed The Button") {
     handlePressCommand(command);
   }
 }
 
-function handlePressCommand(command: CommandInstance): void {
-  
+async function handlePressCommand(command: CommandInstance): Promise<void> {
+  if (!googleAppKey || !googleAppUrl) {
+    logger.log("Missing Google App Url or Key");
+    return;
+  }
+
+  const options: UriOptions & request.RequestPromiseOptions = {
+    body: {
+      app_key: googleAppKey,
+      command: command.command,
+      timestamp: command.dateCreated,
+    },
+    followAllRedirects: true,
+    json: true,
+    method: "POST",
+    uri: googleAppUrl,
+  };
+
+  await request(options)
+    .then((value) => logger.log(value))
+    .catch((err) => logger.log("Error: ", err));
 }
